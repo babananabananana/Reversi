b3f0d3bd07fa39ea11bc646fa2d6f5e942cb675b
Ethan Low <ethanthelow@gmail.com>
2023-02-11 15:43:30 -0700

    complete overhaul

--- reversi_bot.py
+++ reversi_bot.py
@@ -3,20 +3,23 @@ import random as rand
 import reversi
 import copy
 
-# Swap initially from 1/2 to 1/-1,
-# Change flipper to match
-# Run algorithm on a child for opposite
-# Storing as nodes in a tree
+
+# Swap initially from 1/2 to 1/-1,          $
+# Change flipper to match                   $
+# Run algorithm on a child for opposite     $
+# Storing as nodes in a tree                $
 # run a minmax
 # run a/b
 # modify heuristics
+# do we recompile the tree each time? or flesh out the tree a bit, then continue fleshing based on what the opp played  $
 
 
 class ReversiBot:
     def __init__(self, move_num):
-        self.move_num = move_num
-        self.square_vals = np.array([[99,-8,8,6,6,8,-8,99],[-8,-24,-4,-3,-3,-4,-24,-8],[8,-4,7,4,4,7,-4,8],[6,-3,4,0,0,4,-3,6],
-                            [6,-3,4,0,0,4,-3,6],[8,-4,7,4,4,7,-4,8],[-8,-24,-4,-3,-3,-4,-24,-8],[99,-8,8,6,6,8,-8,99]])
+        self.our_turn = move_num
+        self.opponent_turn = 2 if self.our_turn == 1 else 1
+        self.init_state = np.array([[0 for i in range(8)] for j in range(8)])
+        self.game_tree = GameTree(GameNode(reversi.ReversiGameState(self.init_state, self.our_turn), None))
 
     def make_move(self, state):
         '''
@@ -37,42 +40,43 @@ class ReversiBot:
 
         Move should be a tuple (row, col) of the move you want the bot to make.
         '''
-        print('###################################### NEW MOVE ###############')
+        # Parse board into max/min format
+
+        for x in range(len(state.board)):
+            for y in range(len(state.board[0])):
+                if state.board[x][y] == self.our_turn:
+                    state.board[x][y] = 1
+                elif state.board[x][y] == self.opponent_turn:
+                    state.board[x][y] = -1
+
+        # Returns true if all equal, false if any are not
+        x = self.game_tree.find_grandchild_node(state.board)
+
+        # Run algorithm through game tree to pick best child
+        self.game_tree.generate_grandchildren()
+
+
+
+
+
         valid_moves = state.get_valid_moves()
-        self.state = state
-        self.turn = state.turn
-        self.opponent = 2 if state.turn == 1 else 1
+        print(len(valid_moves))
+        return valid_moves[0]
 
-        curr_fin_val = -1000
-        fin_move = valid_moves[0]
 
-        for move in valid_moves:
-            child, tile_count, op_tile_count = self.create_child(state.board, move)
-            mv_val = self.eval_qual(child)
-            if mv_val > curr_fin_val:
-                fin_move = move
 
-        n_state = reversi.ReversiGameState(child, self.opponent)
-        n_val_moves = n_state.get_valid_moves()
 
-        return fin_move
 
-    def create_child(self, board, move, ):
-        state_copy = copy.deepcopy(board)
-        state_copy[move[0]][move[1]] = self.turn
-        tile_count = 0
-        opp_tile_count = 0
-        for x in range(len(state_copy)):
-            for y in range(len(state_copy[0])):
-                if state_copy[x][y] == self.opponent:
-                    state_copy[x][y] = -1
-                    opp_tile_count += 1
-                if state_copy[x][y] == self.turn:
-                    state_copy[x][y] = 1
-                    tile_count += 1
 
-        self.changeColors(move[0], move[1], state_copy)
-        return state_copy, tile_count, opp_tile_count
+
+class GameTree:
+    def __init__(self, root):
+        self.root = root
+        self.curr = root
+        self.square_vals = np.array([[99,-8,8,6,6,8,-8,99],[-8,-24,-4,-3,-3,-4,-24,-8],[8,-4,7,4,4,7,-4,8],[6,-3,4,0,0,4,-3,6],
+                            [6,-3,4,0,0,4,-3,6],[8,-4,7,4,4,7,-4,8],[-8,-24,-4,-3,-3,-4,-24,-8],[99,-8,8,6,6,8,-8,99]])
+        self.generate_grandchildren()
+
 
     def board_heatmap(self, state):
         val = state * self.square_vals
@@ -86,66 +90,65 @@ class ReversiBot:
         fin_val = val
         return fin_val
 
+    def generate_grandchildren(self):
+        self.curr.create_children()
+        for child in self.curr.children:
+            child.create_children()
+
+    def find_grandchild_node(self, board):
+        # Find which GameTree's grandchild matches current game state
+        for child in self.curr.children:
+            for grandchild in child.children:
+                if (grandchild.state.board == board).min():
+                    self.curr = grandchild
+                    return True
+        return False
+
+
+class GameNode:
+    def __init__(self, state, parent_move):
+        self.state = state
+        self.children = []
+        self.valid_moves = self.getValidMoves(state.turn)
+        self.parent_move = parent_move
+        self.opponent_turn = -1 if self.state.turn == 1 else 1
 
 
 
+    def create_children(self):
+        for move in self.valid_moves:
+            child_board = self.create_child(move)
+            self.children.append(GameNode(reversi.ReversiGameState(child_board, self.opponent_turn), move))
 
 
+    def create_child(self, move):
+        state_copy = copy.deepcopy(self.state.board)
+        state_copy[move[0]][move[1]] = self.state.turn
+        tile_count = 0
+        opp_tile_count = 0
 
-    def checkDirection(self, row, col, incx, incy, state):
-        sequence = [0 for _ in range(8)]
+        # for x in range(len(state_copy)):
+        #     for y in range(len(state_copy[0])):
+        #         if state_copy[x][y] == self.opponent:
+        #             state_copy[x][y] = -1
+        #             opp_tile_count += 1
+        #         if state_copy[x][y] == self.state.turn:
+        #             state_copy[x][y] = 1
+        #             tile_count += 1
 
-        seqLen = 0
+        self.changeColors(move[0], move[1], state_copy)
+        return state_copy
 
-        for i in range(1, 8):
-            r = row + incy * i
-            c = col + incx * i
+    def couldBe(self, row, col):
+        for incx in range(-1, 2):
+            for incy in range(-1, 2):
+                if (incx == 0) and (incy == 0):
+                    continue
 
-            if (r < 0) or (r > 7) or (c < 0) or (c > 7):
-                break
+                if self.checkDirection(row, col, incx, incy):
+                    return True
 
-            sequence[i] = state[r][c]
-            seqLen += 1
-
-        count = 0
-        for i in range(seqLen): # TURNS = 1 or 2
-            if self.turn == 1:
-                if sequence[i] == -1:
-                    count += 1
-                else:
-                    if (sequence[i] == 1) and (count > 0):
-                        count = 20
-                        break
-            else:
-                if sequence[i] == 1:
-                    count += 1
-                else:
-                    if (sequence[i] == -1) and (count > 0):
-                        count = 20
-                    break
-
-        if count > 10:
-            if self.turn == 1:
-                i = 1
-                r = row + incy * i
-                c = col + incx * i
-                while state[r][c] == -1:
-                    state[r][c] = 1
-                    i += 1
-                    r = row + incy * i
-                    c = col + incx * i
-
-            else:
-                i = 1
-                r = row + incy * i
-                c = col + incx * i
-                while state[r][c] == 1:
-                    state[r][c] = 2
-                    i += 1
-                    r = row + incy * i
-                    c = col + incx * i
-
-        return state
+        return False
 
     def changeColors(self, row, col, state):
         for incx in range(-1, 2):
@@ -153,6 +156,67 @@ class ReversiBot:
                 if (incx == 0) and (incy == 0):
                     continue
 
-                state = self.checkDirection(row, col, incx, incy, state)
+                if self.checkDirection(row, col, incx, incy):
+                    state = self.flipTiles(row, col, incx, incy, state)
 
         return state
+
+    def flipTiles(self, row, col, incx, incy, state):
+        i = 1
+        r = row + incy * i
+        c = col + incx * i
+        while state[r][c] == self.opponent_turn:
+            state[r][c] = self.state.turn
+            i += 1
+            r = row + incy * i
+            c = col + incx * i
+
+
+        return state
+
+    def checkDirection(self, row, col, incx, incy):
+        sequence = []
+        for i in range(1, 8):
+            r = row + incy * i
+            c = col + incx * i
+
+            if ((r < 0) or (r > 7) or (c < 0) or (c > 7)):
+                break
+
+            sequence.append(self.state.board[r][c])
+
+        count = 0
+        for i in range(len(sequence)):
+            if self.state.turn == 1:
+                if sequence[i] == -1:
+                    count = count + 1
+                else:
+                    if (sequence[i] == 1) and (count > 0):
+                        return True
+                    break
+            else:
+                if sequence[i] == 1:
+                    count = count + 1
+                else:
+                    if (sequence[i] == -1) and (count > 0):
+                        return True
+                    break
+
+        return False
+
+    def getValidMoves(self, me):
+        validMoves = []
+
+        if 0 in np.array(self.state.board)[3:5, 3:5]:
+            for row in range(3, 5):
+                for col in range(3, 5):
+                    if self.state.board[row][col] == 0:
+                        validMoves.append((row, col))
+        else:
+            for i in range(8):
+                for j in range(8):
+                    if self.state.board[i][j] == 0:
+                        if self.couldBe(i, j):
+                            validMoves.append([i, j])
+
+        return validMoves
